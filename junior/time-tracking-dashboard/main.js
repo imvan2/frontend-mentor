835479622e17b578c535fe64d2be39d18778eeda
Van Tu <vtu7410@gmail.com>
2025-04-30 12:08:55 -0700

    Changes to accessibility

--- junior/time-tracking-dashboard/main.js
+++ junior/time-tracking-dashboard/main.js
@@ -83,10 +83,10 @@ const populateDOM = (item, timeline) => {
   const newCard = document.createElement("article");
 
   newCard.innerHTML = `
-  <div id="svg-icon" class="svg-icon" style="background-image: url(../time-tracking-dashboard/images/icon-${titleLowercase}.svg)"></div>
+  <div id="svg-icon-${titleLowercase}" class="svg-icon" style="background-image: url(../time-tracking-dashboard/images/icon-${titleLowercase}.svg)"></div>
           <div class="main-section">
             <button class="btn-other">
-              <p id="title">${item["title"]}</p>
+              <p id="${titleLowercase}">${item["title"]}</p>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 <path
                   d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"
