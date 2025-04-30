50af9f56507db99d3f6112493399f44bfd5fc5d8
Van Tu <vtu7410@gmail.com>
2025-04-30 12:12:00 -0700

    Changed span tag placement

--- junior/time-tracking-dashboard/main.js
+++ junior/time-tracking-dashboard/main.js
@@ -88,12 +88,12 @@ const populateDOM = (item, timeline) => {
             <button class="btn-other">
               <span>
                 <p id="${titleLowercase}">${item["title"]}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path
-                    d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"
-                  />
-                </svg>
               </span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path
+                  d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"
+                />
+              </svg>
             </button>
             <div>
               <h2 class="current-time">${currentTime}</h2>
