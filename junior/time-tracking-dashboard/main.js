7f68c27350a863d116e9a6f118016376135a39f1
Van Tu <vtu7410@gmail.com>
2025-04-30 12:10:43 -0700

    Added span tag

--- junior/time-tracking-dashboard/main.js
+++ junior/time-tracking-dashboard/main.js
@@ -86,12 +86,14 @@ const populateDOM = (item, timeline) => {
   <div id="svg-icon-${titleLowercase}" class="svg-icon" style="background-image: url(../time-tracking-dashboard/images/icon-${titleLowercase}.svg)"></div>
           <div class="main-section">
             <button class="btn-other">
-              <p id="${titleLowercase}">${item["title"]}</p>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                <path
-                  d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"
-                />
-              </svg>
+              <span>
+                <p id="${titleLowercase}">${item["title"]}</p>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path
+                    d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"
+                  />
+                </svg>
+              </span>
             </button>
             <div>
               <h2 class="current-time">${currentTime}</h2>
