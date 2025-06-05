386da4c176e0429f7e18a5d320a960e95b10866c
Van Tu <vtu7410@gmail.com>
2025-06-05 13:51:19 -0700

    Added a title

--- intermediate/password-generator-app/pages/_document.tsx
+++ intermediate/password-generator-app/pages/_document.tsx
@@ -1,17 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
+        <title>Password Generator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body className="antialiased bg-grey-850">
         <Main />
-        <NextScript />
       </body>
     </Html>
   );
